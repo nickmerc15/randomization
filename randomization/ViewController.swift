240d5ff2550474555b626280308b130eccf8dbb7
Nick Merc <nmercadante@zagmail.gonzaga.edu>
2020-01-30 09:51:20 -0800

    Initial Commit

---
+++ randomization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomization
+//
+//  Created by Nicholas Mercadante on 1/30/20.
+//  Copyright © 2020 Nicholas Mercadante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
