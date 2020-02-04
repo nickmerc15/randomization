2216728460eb84bdee1347ab14d60c359666fa94
Nick Merc <nmercadante@zagmail.gonzaga.edu>
2020-02-03 21:49:45 -0800

    Click the 8 ball to have your questions answered!

--- randomization/ViewController.swift
+++ randomization/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+  
+    @IBAction func ballPressed(_ sender: Any) {
+        let messages = ["Yes",
+                        "No",
+                        "Likely So",
+                        "Unlikely"]
+        messageLabel.text = messages.randomElement()
+        
+        //if index == messages.count-1 {
+          //  index = 0
+        //}   else{
+              //  index = index + 1
+        //}
+    }
+    
 
 }
 
